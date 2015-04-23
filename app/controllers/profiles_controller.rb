bdeb6bff2e8f97a2708da0124740c5ae7c650b21
alitopaloglu <ali_t@gwmail.gwu.edu>
2015-04-23 18:14:00 -0400

    api deletions

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,23 +8,23 @@ class ProfilesController < ApplicationController
   def show
     @profile = Profile.find(params[:id])
     @next_profile = Profile.all.sample 
-    splashbase_response = HTTParty.get("http://www.splashbase.co/api/v1/images/search?query=cherry%20blossoms")
-    response_body = splashbase_response.body
-    	 # @debug_example = response_body.inspect
-    response_hash = JSON.parse(response_body, symbolize_names: true)
-    @image_url = response_hash[:images][0][:url]
+  #   splashbase_response = HTTParty.get("http://www.splashbase.co/api/v1/images/search?query=cherry%20blossoms")
+  #   response_body = splashbase_response.body
+  #   	 # @debug_example = response_body.inspect
+  #   response_hash = JSON.parse(response_body, symbolize_names: true)
+  #   @image_url = response_hash[:images][0][:url]
 
-    splashbase_response = HTTParty.get("http://www.splashbase.co/api/v1/images/search?query=wallet")
-    response_body = splashbase_response.body
-    	 # @debug_example = response_body.inspect
-    response_hash = JSON.parse(response_body, symbolize_names: true)
-    @image_url2 = response_hash[:images][0][:url]
+  #   splashbase_response = HTTParty.get("http://www.splashbase.co/api/v1/images/search?query=wallet")
+  #   response_body = splashbase_response.body
+  #   	 # @debug_example = response_body.inspect
+  #   response_hash = JSON.parse(response_body, symbolize_names: true)
+  #   @image_url2 = response_hash[:images][0][:url]
 
-    splashbase_response = HTTParty.get("http://www.splashbase.co/api/v1/images/search?query=laptop")
-    response_body = splashbase_response.body
-    	 # @debug_example = response_body.inspect
-    response_hash = JSON.parse(response_body, symbolize_names: true)
-    @image_url3 = response_hash[:images][0][:url]
-  end
+  #   splashbase_response = HTTParty.get("http://www.splashbase.co/api/v1/images/search?query=laptop")
+  #   response_body = splashbase_response.body
+  #   	 # @debug_example = response_body.inspect
+  #   response_hash = JSON.parse(response_body, symbolize_names: true)
+  #   @image_url3 = response_hash[:images][0][:url]
+  # end
 
 end
